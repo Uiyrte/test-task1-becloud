90831bba4036ceb47b0a22c40780ed3899efbe71
timofey.kurstak <timofey.kurstak@yandex.ru>
2026-09-15 23:53:11 +0300

    Добавлен код тестового задания, настроено окружение pipenv

---
+++ main.py
@@ -0,0 +1,52 @@
+import datetime
+
+# Имитация базы данных в памяти
+USERS_DB = {
+    1: {"name": "Иван", "registered_years": 3, "is_vip": True},
+    2: {"name": "Ольга", "registered_years": 1, "is_vip": False},
+    }
+
+
+def calculate_order_total(user_id, items, discount_code=None):
+    """
+    items: список словарей [{"id": 1, "price": 100,
+    "category": "electronics", "qty": 2}]
+    """
+    user = USERS_DB.get(user_id)
+    if not user:
+        raise ValueError("User not found")
+    
+    total = 0
+    for item in items:
+        # Считаем базовую стоимость товара
+        item_total = item["price"] * item["qty"]
+
+        # Легаси-правило 1: скидка на электронику 5%, если куплено больше 1 штуки
+        if item["category"] == "electronics" and item["qty"] > 1:
+            item_total = item_total * 0.95
+
+        # Легаси-правило 2: скидка на книги 10% всегда
+        if item["category"] == "books":
+            item_total = item_total * 0.90
+
+        total += item_total
+
+    # Применение скидок пользователя
+    if user["is_vip"]:
+        total -= total * 0.10 # VIP скидка 10%
+    elif user["registered_years"] >= 3:
+        total -= total * 0.05 # Скидка за лояльность 5%
+
+    # Старая кривая логика промокодов
+    if discount_code == "NEWYEAR2025":
+    # Промокод работает только в декабре и январе (захардкожено)
+        now = datetime.datetime.now()
+        if now.month == 12 or now.month == 1:
+            total -= 500 # Скидка 500 рублей
+    elif discount_code == "SUMMER":
+        total = total * 0.85 # Скидка 15%
+
+    if total < 0:
+        total = 0
+
+    return total
