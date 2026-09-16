c34b30a9693b2a2c78e400307143d828ba25fc26
timofey.kurstak <timofey.kurstak@yandex.ru>
2026-09-16 14:14:08 +0300

    Замен условных конструкция на паттерн Стратегия с регистрами (типы скидок)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import datetime
 from typing import Optional, TypedDict
+from abc import ABC, abstractmethod
+
 
 class User(TypedDict):
     name: str
     registered_years: int
     is_vip: bool
+
 
 class Item(TypedDict):
     id: int
@@ -12,6 +15,98 @@ class Item(TypedDict):
     category: str
     qty: int
 
+
+class CategoryItemSale(ABC):
+    @abstractmethod
+    def calculate_item_cost(self, item: Item) -> float:
+        pass
+
+
+class ElectronicsItemSale(CategoryItemSale):
+    def calculate_item_cost(self, item: Item) -> float:
+        if item["qty"] > 1:
+            return item["price"] * item["qty"] * 0.95
+        return item["price"] * item["qty"]
+
+
+class BooksItemSale(CategoryItemSale):
+    def calculate_item_cost(self, item: Item) -> float:
+        return item["price"] * item["qty"] * 0.9
+
+
+class DefaultItemSale(CategoryItemSale):
+    def calculate_item_cost(self, item: Item) -> float:
+        return item["price"] * item["qty"]
+
+
+ITEM_SALE_REGISTRY: dict[str, CategoryItemSale] = {
+    "electronics": ElectronicsItemSale(),
+    "books": BooksItemSale(),
+}
+DEFAULT_ITEM_SALE: CategoryItemSale = DefaultItemSale()
+
+
+def get_item_sale_strategy(category: str) -> CategoryItemSale:
+    return ITEM_SALE_REGISTRY.get(category, DEFAULT_ITEM_SALE)
+
+
+class PersonSale(ABC):
+    @abstractmethod
+    def is_available(self, user: User) -> bool:
+        pass
+
+    @abstractmethod
+    def calculate_total_cost(self, total: float) -> float:
+        pass
+
+
+class VipPersonSale(PersonSale):
+    def is_available(self, user: User) -> bool:
+        if user["is_vip"]:
+            return True
+        return False
+
+    def calculate_total_cost(self, total: float) -> float:
+        return total * 0.9
+
+
+class LoyalPersonSale(PersonSale):
+    def is_available(self, user: User) -> bool:
+        if user["registered_years"] >= 3:
+            return True
+        return False
+
+    def calculate_total_cost(self, total: float) -> float:
+        return total * 0.95
+
+
+PERSON_SALE_REGISTRY: list[PersonSale] = [VipPersonSale(), LoyalPersonSale()]
+
+
+class DiscountCodeSale(ABC):
+    @abstractmethod
+    def calculate_total_cost(self, total: float) -> float:
+        pass
+
+
+class NewYearCodeSale(DiscountCodeSale):
+    def calculate_total_cost(self, total: float) -> float:
+        now: datetime.datetime = datetime.datetime.now()
+        if now.month == 12 or now.month == 1:
+            total -= 500
+        return total
+
+
+class SummerCodeSale(DiscountCodeSale):
+    def calculate_total_cost(self, total: float) -> float:
+        total = total * 0.85  # Скидка 15%
+        return total
+
+
+CODE_SALE_REGISTRY: dict[str, DiscountCodeSale] = {
+    "NEWYEAR2025": NewYearCodeSale(),
+    "SUMMER": SummerCodeSale(),
+}
 
 # Имитация базы данных в памяти
 USERS_DB: dict[int, User] = {
@@ -26,7 +121,9 @@ USERS_DB: dict[int, User] = {
 }
 
 
-def calculate_order_total(user_id: int, items: list[Item], discount_code: Optional[str] = None) -> float:
+def calculate_order_total(
+    user_id: int, items: list[Item], discount_code: Optional[str] = None
+) -> float:
     """
     items: список словарей [{"id": 1, "price": 100,
     "category": "electronics", "qty": 2}]
@@ -37,36 +134,23 @@ def calculate_order_total(user_id: int, items: list[Item], discount_code: Option
 
     total: float = 0
 
-    item:  Item
+    item: Item
     for item in items:
-        # Считаем базовую стоимость товара
-        item_total: float = item["price"] * item["qty"]
-
-        # Легаси-правило 1:
-        # скидка на электронику 5%, если куплено больше 1 штуки
-        if item["category"] == "electronics" and item["qty"] > 1:
-            item_total = item_total * 0.95
-
-        # Легаси-правило 2: скидка на книги 10% всегда
-        if item["category"] == "books":
-            item_total = item_total * 0.90
-
-        total += item_total
+        # Применение скидок, соответствующих категории, количетсву и т.п.
+        item_sale_strategy: CategoryItemSale = get_item_sale_strategy(
+            item["category"]
+        )
+        total += item_sale_strategy.calculate_item_cost(item)
 
     # Применение скидок пользователя
-    if user["is_vip"]:
-        total -= total * 0.10  # VIP скидка 10%
-    elif user["registered_years"] >= 3:
-        total -= total * 0.05  # Скидка за лояльность 5%
+    for person_sale in PERSON_SALE_REGISTRY:
+        if person_sale.is_available(user):
+            total = person_sale.calculate_total_cost(total)
+            break
 
-    # Старая кривая логика промокодов
-    if discount_code == "NEWYEAR2025":
-        # Промокод работает только в декабре и январе (захардкожено)
-        now: datetime.datetime = datetime.datetime.now()
-        if now.month == 12 or now.month == 1:
-            total -= 500  # Скидка 500 рублей
-    elif discount_code == "SUMMER":
-        total = total * 0.85  # Скидка 15%
+    # Применение скидок промокодов
+    if discount_code and discount_code in CODE_SALE_REGISTRY:
+        total = CODE_SALE_REGISTRY[discount_code].calculate_total_cost(total)
 
     if total < 0:
         total = 0
